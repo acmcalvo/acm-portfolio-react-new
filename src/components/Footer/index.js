c35b3b63c31b538167f11fef10a99a3d862fc628
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-03-11 01:53:44 -0500

    try find error childen with same key

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import Octocat from "../assets/Octocat.png";
 
-function Footer() {   
-    return (
-    <footer className="footer  bg-secondary fixed-bottom">
-        <a href="https://github.com/acmcalvo"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
-        <a href="www.linkedin.com/in/alvaro-calvo-acm"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer className="footer bg-secondary sticky-botton">
+      <a href="https://github.com/acmcalvo">
+        <img
+          src={Octocat} width="100" height="100"
+          alt="Github"
+          className="icon"
+        />
+      </a>
+      <a href="www.linkedin.com/in/alvaro-calvo-acm">
+        <img
+          src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+          alt="LinkedIn"
+          className="icon"
+        />
+      </a>
+    </footer>
+  );
 }
 
 export default Footer;
