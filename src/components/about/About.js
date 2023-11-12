a796c5d2a15dc1567ee5204cd6756d61a668eba5
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2023-11-12 16:51:20 -0500

    update About me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -16,27 +16,21 @@ function About() {
         <div>
           
           <p>
-          I was born and raised in San Jose, Costa Rica, for most of my childhood I live in a small town known as "Curridabat.
-          " I attended High School in the same town that I met the love of my life.
+          To achieve highly functional and efficient communication systems through creative and collaborative developments 
+          in modern workflow technologies. To continue to seek solutions for corporate integration into advancing software and
+           production systems and needs.
            
           </p>
           <p>
-          We get married, and we have two daughters and two sons, After we got married, we moved to America to explore how to leave different counties Plus, my wife was born in the United States.
+          30  years of print, networking, and prepress experience
+Extremely proficient across both Mac and PC platforms
+Exceptionally mindful of details, steady and committed work ethic, solid, consistent, focused
+Excited about dissecting and solving problems and formulating and implementing new, efficient, and cost-effective solutions
+Passionate about remaining up-to-date on current software advancements and trends
+Trained and certified in HP digital equipment color correction
+Bilingual with advanced fluency in Spanish
           </p>
-          <p>
-          As early as 2000, we returned to Costa Rica, I started working at Advertising Company Sepacolor,  "Sepacolor" 
-          change my carrier, and let me get more in the technical side of "Graphics Design," so pursue grow in my 
-          carrier take the chance to get another job in Tribu Saatchi & Saatchi, now called Havas Costa Rica, is a great place 
-          to work and a very professional workplace, Watching my three kids grow up. We always like being parents and we want 
-          to give my children the best we move back to the USA, My eldest son was suffocated by all the changes at that time, 
-          my old hope he missed so, they started to grow up here So, they started to grow here. Then, In order to go to college, 
-          my son move to Daytona Beach, and my daughter decided to join the army And young people still want to have a home in high school. 
-          So .. Something happened, we got a new member in our family, in the mean time My son graduated from university and became AeroSpaces Enginer ,
-           and my daughter She has just completed her deployment and changed hands into the car dealership business. My old song was pursued in college.
-           
-           
-          </p>
-        
+      
         </div>
       </div>
     </section>
